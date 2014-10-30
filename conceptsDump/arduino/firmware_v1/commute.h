549e8ca3336b42a67a67f70c8751a8b84302684e
kjph <jme.ph2@gmail.com>
2014-10-31 00:28:08 +0800

    firmware (v1) update. Need to add PWM freq nd make sure MOSFETS are switched off after use

---
+++ conceptsDump/arduino/firmware_v1/commute.h
@@ -0,0 +1,5 @@
+#ifndef commute_h
+#define commute_h
+
+
+#endif
--- conceptsDump/arduino/firmware_v1/test.c
+++ conceptsDump/arduino/firmware_v1/test.c
@@ -1,7 +1,12 @@
 //Hall sensor inputs
-int hallA;
-int hallB;
-int hallC;
+int hallA_sensor;
+int hallB_sensor;
+int hallC_sensor;
+
+//Initialize Hall sensor state variables
+int hallA_state = 0;
+int hallB_state = 0;
+int hallC_state = 0;
 
 //MOSFETS output
 int pinA_low;
@@ -13,15 +18,19 @@ int pinB_high;
 int pinC_high;
 
 //MODIFIER INPUT
-int modifier;
+int pinSensor;
+int speed = 0;
+int correction;
+
+int ANALOG_WRITE_READ = 1024/256;
 
 void setup(void)
 {
-    pinMode(hallA, INPUT);
-    pinMode(hallB, INPUT);
-    pinMode(hallC, INPUT);
-
-    pinMode(modifier, INPUT);
+    pinMode(hallA_sensor, INPUT);
+    pinMode(hallB_sensor, INPUT);
+    pinMode(hallC_sensor, INPUT);
+    pinMode(correction, INPUT);
+    pinMode(pinSensor, INPUT);
 
     pinMode(pinA_low, OUTPUT);
     pinMode(pinB_low, OUTPUT);
@@ -34,22 +43,41 @@ void setup(void)
 
 void loop(void)
 {
-    if (hallA == 0 && hallB == 0 && hallC == 0) {
-        //000
-    } else if (hallA == = 0 && hallB == 0 && hallC == 1) {
-        //001
-    } else if (hallA == = 0 && hallB == 1 && hallC == 0) {
-        //010
-    } else if (hallA == = 0 && hallB == 1 && hallC == 1) {
-        //011
-    } else if (hallA == = 1 && hallB == 0 && hallC == 0) {
-        //100
-    } else if (hallA == = 1 && hallB == 0 && hallC == 1) {
-        //101
-    } else if (hallA == = 1 && hallB == 1 && hallC == 0) {
-        //110
-    } else if (hallA == = 1 && hallB == 1 && hallC == 1) {
-        //111
+    speed = analogRead(pinSensor)/ANALOG_WRITE_READ;
+
+    //Read the hall sensor values
+    hallA_state = digitalRead(hallA_sensor);
+    hallB_state = digitalRead(hallB_sensor);
+    hallC_state = digitalRead(hallC_sensor);
+
+    //Hopefully clockwise commutation
+    if (hallA_state == 0 && hallB_state == 0 && hallC_state == 0) { //000
+        analogWrite(pinA_low, 0);
+        analogWrite(pinB_low, 0);
+        analogWrite(pinC_low, 0);
+        analogWrite(pinA_high, 0);
+        analogWrite(pinB_high, 0);
+        analogWrite(pinC_high, 0);
+    } else if (hallA_state == 0 && hallB_state == 0 && hallC_state == 1) { //001
+        analogWrite(pinC_high, speed);
+        analogWrite(pinB_low, 255);
+    } else if (hallA_state == 0 && hallB_state == 1 && hallC_state == 0) { //010
+        analogWrite(pinB_high, speed);
+        analogWrite(pinA_low, 255);
+    } else if (hallA_state == 0 && hallB_state == 1 && hallC_state == 1) { //011
+        analogWrite(pinC_high, speed);
+        analogWrite(pinA_low, 255);
+    } else if (hallA_state == 1 && hallB_state == 0 && hallC_state == 0) { //100
+        analogWrite(pinA_high, speed);
+        analogWrite(pinC_low, 255);
+    } else if (hallA_state == 1 && hallB_state == 0 && hallC_state == 1) { //101
+        analogWrite(pinA_high, speed);
+        analogWrite(pinB_low, 255);
+    } else if (hallA_state == 1 && hallB_state == 1 && hallC_state == 0) { //110
+        analogWrite(pinB_high, speed);
+        analogWrite(pinC_low, 255);
+    } else if (hallA_state == 1 && hallB_state == 1 && hallC_state == 1) { //111
+        //error
     } else {
         //error
     }
